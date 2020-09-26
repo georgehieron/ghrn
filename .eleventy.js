edecc07372517ec0b6f90ace6794d59f717d31f1
George Hieron <george.hieron@gmail.com>
2020-09-26 21:38:18 +0300

    Replace Moment with Luxon. Remove w3 date filter

--- .eleventy.js
+++ .eleventy.js
@@ -4,7 +4,6 @@ const rssPlugin = require("@11ty/eleventy-plugin-rss");
 
 // Filters
 const dateFilter = require("./src/filters/date-filter.js");
-const w3DateFilter = require("./src/filters/w3-date-filter.js");
 
 // Transforms
 const htmlMinTransform = require('./src/transforms/html-min-transform.js');
@@ -22,7 +21,6 @@ module.exports = (config) => {
 
     // Add filters
     config.addFilter("dateFilter", dateFilter);
-    config.addFilter("w3DateFilter", w3DateFilter);
 
     // Only minify HTML if we are in production because it slows builds _right_ down
     if (isProduction) {
--- src/filters/date-filter.js
+++ src/filters/date-filter.js
@@ -1,6 +1,6 @@
-const moment = require("moment");
+const { DateTime } = require("luxon");
 
 module.exports = (value) => {
-    const dateObject = moment(value);
-    return `${dateObject.format("Do")} of ${dateObject.format("MMMM YYYY")}`;
+    const dateObject = DateTime.fromISO(value).toFormat('dd LLLL yyyy');
+    return dateObject;
 };
