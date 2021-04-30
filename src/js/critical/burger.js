6fc02b9f7f0fe32d7eeb49b75a0e3e78f12d7400
George Hieron <george.hieron@gmail.com>
2021-04-30 22:21:03 +0300

    Add label to burger, with translation. Update main menu indicator

--- src/js/critical/burger.js
+++ src/js/critical/burger.js
@@ -2,23 +2,36 @@ document.addEventListener(
     "DOMContentLoaded",
     function () {
         const menuBrgr = document.getElementById("js-brgr");
+        const brgrLabel = document.getElementById("js-brgr-label");
+        const brgrTxtOpen = brgrLabel.querySelector(".open");
+        const brgrTxtClose = brgrLabel.querySelector(".close");
+        const brgrTxtMenu = brgrLabel.querySelector(".menu");
+
+        console.log(brgrTxtOpen);
 
         /**
          * Get’s the current setting > reverses it > sets it
          */
         const toggleBrgr = () => {
             let currentSetting = menuBrgr.getAttribute("aria-expanded");
+            let oppositeSetting;
 
             switch (currentSetting) {
                 case "false":
                     currentSetting = "true";
+                    oppositeSetting = "false";
                     break;
                 case "true":
                     currentSetting = "false";
+                    oppositeSetting = "true";
                     break;
             }
 
             menuBrgr.setAttribute("aria-expanded", currentSetting);
+            brgrTxtOpen.setAttribute("aria-hidden", currentSetting);
+            brgrTxtClose.setAttribute("data-hide", oppositeSetting);
+            brgrTxtClose.setAttribute("aria-hidden", oppositeSetting);
+            brgrTxtMenu.setAttribute("data-hide", currentSetting);
             document.documentElement.setAttribute("data-menu-open", currentSetting);
         };
 
