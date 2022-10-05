67e630c92b6f4c0379309a2e538dcc6aae79c6ed
George Hieron <george.hieron@gmail.com>
2022-10-05 21:46:47 +0300

    Menu accessibility improvements

--- src/js/critical/burger.js
+++ src/js/critical/burger.js
@@ -7,6 +7,8 @@ document.addEventListener(
         const brgrTxtClose = brgrLabel.querySelector(".close");
         const brgrTxtMenu = brgrLabel.querySelector(".menu");
         const skipLink = document.getElementById("js-skip-link");
+        const siteHead = document.getElementById("site-head");
+        const htmlEl = document.querySelector( 'html' );
 
         /**
          * Get’s the current setting > reverses it > sets it
@@ -32,8 +34,12 @@ document.addEventListener(
             brgrTxtClose.setAttribute("data-hide", oppositeSetting);
             brgrTxtClose.setAttribute("aria-hidden", oppositeSetting);
             brgrTxtMenu.setAttribute("data-hide", currentSetting);
-            document.documentElement.setAttribute("data-menu-open", currentSetting);
-            setTimeout(function() {
+            document.documentElement.setAttribute(
+                "data-menu-open",
+                currentSetting
+            );
+            trapFocus(siteHead);
+            setTimeout(function () {
                 brgrBtn.removeAttribute("disabled");
             }, 333);
         };
@@ -51,6 +57,34 @@ document.addEventListener(
             brgrTxtClose.setAttribute("aria-hidden", true);
             brgrTxtMenu.setAttribute("data-hide", false);
             document.documentElement.setAttribute("data-menu-open", "false");
+            brgrBtn.focus();
+        }
+
+        function trapFocus(el) {
+            var focusableEls = el.querySelectorAll('a[href]:not([disabled]), button');
+            var firstFocusableEl = focusableEls[0];
+            var lastFocusableEl = focusableEls[focusableEls.length - 1];
+            var KEYCODE_TAB = 9;
+
+            el.addEventListener('keydown', function (e) {
+                var isTabPressed = (e.key === 'Tab' || e.keyCode === KEYCODE_TAB);
+
+                if (!isTabPressed) {
+                    return;
+                }
+
+                if (e.shiftKey) /* shift + tab */ {
+                    if (document.activeElement === firstFocusableEl) {
+                        lastFocusableEl.focus();
+                        e.preventDefault();
+                    }
+                } else /* tab */ {
+                    if (document.activeElement === lastFocusableEl) {
+                        firstFocusableEl.focus();
+                        e.preventDefault();
+                    }
+                }
+            });
         }
 
         /**
@@ -61,6 +95,14 @@ document.addEventListener(
             evt.preventDefault();
 
             toggleBrgr();
+        });
+
+        document.addEventListener("keyup", (evt) => {
+            evt.preventDefault();
+
+            if (evt.key === 'Escape' && htmlEl.dataset.menuOpen === 'true') {
+                closeBrgr();
+            }
         });
 
         skipLink.addEventListener("click", () => {
